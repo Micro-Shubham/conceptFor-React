b471b5c53835a5702dac6220cb92feca53e00873
Shubham Yadav <shubhamtikkha09@gmail.com>
2025-04-05 23:01:59 +0530

    Refactor comments in Promise example for clarity and consistency

--- concept for react/Promise.js
+++ concept for react/Promise.js
@@ -6,7 +6,9 @@ const event = new Promise((resolve, reject) => {
         reject("Name was not Pedro, name was:"+name)
     }
 })
+// every promise in js contain then and catch
 // then is for resolve and catch is for reject
+// lets grab our promise which event 
 event.then((name) =>{
 console.log(name) 
 }).catch((err) => {
