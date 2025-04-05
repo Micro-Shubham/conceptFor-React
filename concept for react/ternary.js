8860f9f8e33333f9c1ced95e0e952557246182cb
Shubham Yadav <shubhamtikkha09@gmail.com>
2025-04-05 16:32:25 +0530

    Add initial implementation of ternary operator example

---
+++ concept for react/ternary.js
@@ -0,0 +1,5 @@
+// its hard to write long if and else statment in ui
+// To make is easy and handy ternary operator comes in place
+let age = 20;
+let adultOrNot = age > 18 ? "Adult " : "teenager";
+console.log(adultOrNot);
