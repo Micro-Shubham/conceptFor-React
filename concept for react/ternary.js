aacd0240761bfb2f3e9e36c373642820eeb03023
Shubham Yadav <shubhamtikkha09@gmail.com>
2025-04-05 16:38:36 +0530

    Add example of using ternary operator in a React component

--- concept for react/ternary.js
+++ concept for react/ternary.js
@@ -3,3 +3,7 @@
 let age = 20;
 let adultOrNot = age > 18 ? "Adult " : "teenager";
 console.log(adultOrNot);
+// lets see how it will be handy in react componet 
+const component = () => {
+    return age > 18 ? <div>Adult</div> : <div>Teenager</div>
+}
