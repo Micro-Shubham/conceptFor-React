42e05b6b97ef0f981d19bed6be34f2e2915d7365
Shubham Yadav <shubhamtikkha09@gmail.com>
2025-04-05 15:37:08 +0530

    Add initial implementation of function definitions using traditional and arrow function syntax

---
+++ concept for react/arrow.js
@@ -0,0 +1,12 @@
+// old way to define a function
+// if want to export this to 
+
+export default function doSomething ()
+
+{
+    // perform some task 
+}
+// new way is arrow function and this is use in react while defining components
+export const domsome = () => {
+    // perform some task 
+}
